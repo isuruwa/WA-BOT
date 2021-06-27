f8d46d5ffceb2eb72821b23266983d1fa6875996
Devil Master <72663288+isuruwa@users.noreply.github.com>
2021-06-27 17:56:49 +0530

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,54 @@
+const help = (prefix) => {
+	return `🤡 HELLO ! 🤡
+	👻 I'm A WHATSAPP BOT REDEVELOPED BY DEVL MASTER 👻
+	
+> User Commands <
+	
+!sticker -  convert image/gif/video to sticker (reply to image/video)
+
+!toimg -  convert sticker to image (reply to sticker)
+
+!info - About BOT
+
+!tts - convert text to audio |  gtts [cc] [text]
+
+!ocr -  Take the text in the picture
+
+!setprefix - ${prefix}setprefix [text|optional]
+
+!bc - print who are you
+
+!clone - Clone a Target number profile pic to Yours 
+
+!welcome - Activate Group Welcome
+
+!clearall - Delete all chat
+
+
+> Group Comands <
+
+!add -  add member into group
+
+!kick - kick members from group
+
+!promote - promote a member
+
+!demote - demote a member
+
+!linkgroup - Get the group link
+
+!listadmins - List Admins in group
+
+!leave - Leave the bot from group
+
+!tagall -  Tag All Members
+
+!blocklist - List Of Blocked Numbers
+
+
+____________________________________
+
+BOT REDEVELOPED BY DEVIL MASTER - https://github.com/isuruwa`
+}
+
+exports.help = help
